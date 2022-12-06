7bb5f2f74514aba0ed1994213258bfc5cf95e25d
Krutyi-4el <menorca@meta.ua>
2022-12-06 16:07:28 +0200

    improve memoization

--- i18n/loaders/loader.py
+++ i18n/loaders/loader.py
@@ -14,7 +14,7 @@ class I18nFileLoadError(Exception):
 class Loader(object):
     """Base class to load resources"""
 
-    loaded_files = set()
+    loaded_files = {}
 
     def __init__(self):
         super(Loader, self).__init__()
@@ -27,23 +27,31 @@ class Loader(object):
             raise I18nFileLoadError("error loading file {0}: {1}".format(filename, e.strerror)) from e
 
     def parse_file(self, file_content):
-        raise NotImplementedError("the method parse_file has not been implemented for class {0}".format(self.__class__.name__))
+        raise NotImplementedError("the method parse_file has not been implemented for class {0}".format(self.__class__.__name__))
 
     def check_data(self, data, root_data):
         return True if root_data is None else root_data in data
 
     def get_data(self, data, root_data):
-        return data if root_data is None else data[root_data]
+        # use .pop to remove used data from cache
+        return data if root_data is None else data.pop(root_data)
 
-    def load_resource(self, filename, root_data):
+    def load_resource(self, filename, root_data, remember_content):
         filename = os.path.abspath(filename)
         if filename in self.loaded_files:
-            return {}
-        file_content = self.load_file(filename)
-        data = self.parse_file(file_content)
+            data = self.loaded_files[filename]
+            if not data:
+                # cache is missing or exhausted
+                return {}
+        else:
+            file_content = self.load_file(filename)
+            data = self.parse_file(file_content)
         if not self.check_data(data, root_data):
             raise I18nFileLoadError("error getting data from {0}: {1} not defined".format(filename, root_data))
         enable_memoization = config.get('enable_memoization')
         if enable_memoization:
-            self.loaded_files.add(filename)
+            if remember_content:
+                self.loaded_files[filename] = data
+            else:
+                self.loaded_files[filename] = None
         return self.get_data(data, root_data)
