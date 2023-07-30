dcd9f9e2398838600127d655c6173fdf478216ae
Krutyi-4el <60041069+Krutyi-4el@users.noreply.github.com>
2023-07-30 23:24:23 +0300

    v0.7 (#14)
    * exclude test scripts from distribution
    
    * add maintainer field
    
    * update classifiers
    
    * remove unneeded argument
    
    * add package installation to tests
    
    * bump version

--- setup.py
+++ setup.py
@@ -2,29 +2,30 @@ from setuptools import setup
 
 setup(
     name='i18nice',
-    version='0.6.2',
+    version='0.7.0',
     description='Translation library for Python',
     long_description=open('README.md').read(),
     long_description_content_type='text/markdown',
     author='Daniel Perez',
     author_email='tuvistavie@gmail.com',
+    maintainer="Krutyi 4el",
     url='https://github.com/Krutyi-4el/i18nice',
     download_url='https://github.com/Krutyi-4el/i18nice/archive/master.zip',
     license='MIT',
-    packages=['i18n', 'i18n.loaders', 'i18n.tests'],
-    include_package_data=True,
+    packages=['i18n', 'i18n.loaders'],
     zip_safe=True,
     test_suite='i18n.tests',
     extras_require={
         'YAML': ["pyyaml>=3.10"],
     },
     classifiers=[
-        'Development Status :: 4 - Beta',
+        'Development Status :: 5 - Production/Stable',
         'Environment :: Other Environment',
         'Intended Audience :: Developers',
         'License :: OSI Approved :: MIT License',
         'Operating System :: OS Independent',
         'Programming Language :: Python :: 3',
-        'Topic :: Software Development :: Libraries'
+        'Topic :: Software Development :: Libraries',
+        'Topic :: Software Development :: Internationalization',
     ],
 )
