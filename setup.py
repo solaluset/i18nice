a4094b3be78c44d70d30e83fe0bdc5235ad549e7
Krutyi-4el <60041069+Krutyi-4el@users.noreply.github.com>
2023-08-15 19:47:58 +0300

    Fix links on PyPI (#19)
    * fix links on PyPI
    
    * bump version

--- setup.py
+++ setup.py
@@ -1,5 +1,4 @@
 import re
-from urllib.parse import urljoin
 
 from setuptools import setup
 
@@ -9,13 +8,13 @@ long_description = open("README.md").read()
 # links on PyPI should have absolute URLs
 long_description = re.sub(
     r"(\[[^\]]+\]\()((?!https?:)[^\)]+)(\))",
-    lambda m: m.group(1) + urljoin(GITHUB_URL, "blob/master/" + m.group(2)) + m.group(3),
+    lambda m: m.group(1) + GITHUB_URL + "/blob/master/" + m.group(2) + m.group(3),
     long_description,
 )
 
 setup(
     name='i18nice',
-    version='0.9.0',
+    version='0.9.1',
     description='Translation library for Python',
     long_description=long_description,
     long_description_content_type='text/markdown',
@@ -23,7 +22,7 @@ setup(
     author_email='tuvistavie@gmail.com',
     maintainer="Krutyi 4el",
     url=GITHUB_URL,
-    download_url=urljoin(GITHUB_URL, "archive/master.zip"),
+    download_url=GITHUB_URL + "/archive/master.zip",
     license='MIT',
     packages=['i18n', 'i18n.loaders'],
     zip_safe=True,
