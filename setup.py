54ba61b33de0482c295f0d2ab51fc0c0228a2476
Krutyi 4el <60041069+Krutyi-4el@users.noreply.github.com>
2023-09-24 11:47:16 +0300

    bump version

--- setup.py
+++ setup.py
@@ -14,7 +14,7 @@ long_description = re.sub(
 
 setup(
     name='i18nice',
-    version='0.9.1',
+    version='0.10.0',
     description='Translation library for Python',
     long_description=long_description,
     long_description_content_type='text/markdown',
