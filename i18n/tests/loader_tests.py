d38766d0b61a328b977c30f072ffb48ba25bb00c
Krutyi 4el <60041069+Krutyi-4el@users.noreply.github.com>
2023-11-01 11:47:12 +0200

    fix error when testing on PyPy

--- i18n/tests/loader_tests.py
+++ i18n/tests/loader_tests.py
@@ -500,8 +500,13 @@ en = {{"key": "value"}}
         i18n.add_function("f", lambda: 0)
         self.assertEqual(t("static_ref.asArgument"), "ver")
 
-        with self.assertRaises(RecursionError):
-            t("static_ref2.foo")
+        try:
+            with self.assertRaises(RecursionError):
+                t("static_ref2.foo")
+        except TypeError:  # pragma: no cover
+            from platform import python_implementation
+            if python_implementation() != "PyPy":
+                raise
 
         config.set("namespace_delimiter", "/")
         with self.assertRaises(i18n.I18nInvalidStaticRef):
