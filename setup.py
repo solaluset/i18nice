ae01c0259e74d321218af48126da3ae0967dccad
Sóla Lusøt <60041069+solaluset@users.noreply.github.com>
2024-05-03 23:50:52 +0300

    Change args in custom functions (#54)
    * pass custom function arguments separately
    
    * update README
    
    * bump version

--- setup.py
+++ setup.py
@@ -14,7 +14,7 @@ long_description = re.sub(
 
 setup(
     name='i18nice',
-    version="0.14.1",
+    version="0.15.0",
     description='Translation library for Python',
     long_description=long_description,
     long_description_content_type='text/markdown',
