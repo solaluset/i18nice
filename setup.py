c0034e354dd833e5ee8feb5d792345abf02c00b0
Sóla Lusøt <60041069+solaluset@users.noreply.github.com>
2024-05-06 16:01:54 +0300

    bump version (#58)

--- setup.py
+++ setup.py
@@ -14,7 +14,7 @@ long_description = re.sub(
 
 setup(
     name='i18nice',
-    version="0.15.0",
+    version="0.15.1",
     description='Translation library for Python',
     long_description=long_description,
     long_description_content_type='text/markdown',
