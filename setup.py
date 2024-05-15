570540c1a8144ba00344b1eff2860e22c3ec9895
solaluset <60041069+solaluset@users.noreply.github.com>
2024-05-15 04:17:56 +0300

    bump version

--- setup.py
+++ setup.py
@@ -14,7 +14,7 @@ long_description = re.sub(
 
 setup(
     name='i18nice',
-    version="0.15.3",
+    version="0.15.4",
     description='Translation library for Python',
     long_description=long_description,
     long_description_content_type='text/markdown',
